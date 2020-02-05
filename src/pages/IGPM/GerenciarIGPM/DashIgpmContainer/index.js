56b46eef223a9f15130241bc422451165c9a2365
guilhermestain <guilhermestain@gmail.com>
2020-02-05 17:33:00 -0300

    add table gerenciar igpm

--- src/pages/IGPM/GerenciarIGPM/DashIgpmContainer/index.js
+++ src/pages/IGPM/GerenciarIGPM/DashIgpmContainer/index.js
@@ -3,11 +3,148 @@ import "../../../../global.css";
 import "./index.css";
 
 class DashIgmpContainer extends Component {
+  state = {
+    nome: "",
+    data: "",
+    nContrato: "",
+    total: 10,
+    count: 0,
+    page: 3
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  TableIgpm = () => (
+    <div className="div-table">
+      <div className="div-main-table">
+        <div className="div-line-table">
+          <label className="label-nome-igpm">TESTE TESTE TESTE</label>
+          <label className="label-data-igpm">FEVEREIRO</label>
+          <label className="label-nContrato-igpm">7563518</label>
+        </div>
+      </div>
+    </div>
+  );
+
+  Pages = () => (
+    <div className="div-pages">
+      {Math.ceil(this.state.count / this.state.total) >= 5 &&
+      Math.ceil(this.state.count / this.state.total) - this.state.page < 1 ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page - 4)}
+        >
+          {this.state.page - 4}
+        </button>
+      ) : null}
+      {Math.ceil(this.state.count / this.state.total) >= 4 &&
+      Math.ceil(this.state.count / this.state.total) - this.state.page < 2 &&
+      this.state.page > 3 ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page - 3)}
+        >
+          {this.state.page - 3}
+        </button>
+      ) : null}
+      {this.state.page >= 3 ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page - 2)}
+        >
+          {this.state.page - 2}
+        </button>
+      ) : null}
+      {this.state.page >= 2 ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page - 1)}
+        >
+          {this.state.page - 1}
+        </button>
+      ) : null}
+      <div className="div-teste">{this.state.page}</div>
+      {this.state.page < this.state.count / this.state.total ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page + 1)}
+        >
+          {this.state.page + 1}
+        </button>
+      ) : null}
+      {this.state.page + 1 < this.state.count / this.state.total ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page + 2)}
+        >
+          {this.state.page + 2}
+        </button>
+      ) : null}
+      {this.state.page + 2 < this.state.count / this.state.total &&
+      this.state.page < 3 ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page + 3)}
+        >
+          {this.state.page + 3}
+        </button>
+      ) : null}
+      {this.state.page + 3 < this.state.count / this.state.total &&
+      this.state.page < 2 ? (
+        <button
+          className="button-salvar"
+          type="primary"
+          onClick={() => this.changePages(this.state.page + 4)}
+        >
+          {this.state.page + 4}
+        </button>
+      ) : null}
+    </div>
+  );
+
   render() {
     return (
       <div className="card-main">
         <div className="div-titulo">
           <h1 className="h1-titulo">Gerenciar igpm</h1>
+        </div>
+        <div className="div-inputs-flex">
+          <input
+            className="input-nome-igpm"
+            placeholder="RAZÃO SOCIAL / NOME"
+            onChange={this.onChange}
+            name="nome"
+            value={this.state.nome}
+          ></input>
+          <input
+            className="input-data-igpm"
+            placeholder="DATA"
+            onChange={this.onChange}
+            name="data"
+            value={this.state.data}
+          ></input>
+          <input
+            className="input-nContato-igpm"
+            placeholder="Nº CONTRATO"
+            onChange={this.onChange}
+            name="nContrato"
+            value={this.state.nContrato}
+          ></input>
+        </div>
+        <this.TableIgpm />
+        <div className="div-main-pages">
+          <this.Pages />
         </div>
       </div>
     );
